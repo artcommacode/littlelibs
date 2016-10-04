dc93bf32482ac233125e3c8583ada13ba8a43439
Ryan <ryan@superpositionkitty.com>
2016-10-04 18:48:15 +0100

    fix fnull test

--- src/fnull/test.js
+++ src/fnull/test.js
@@ -6,6 +6,6 @@ test('fnull', (t) => {
   const div = (a, b) => a / b
   const div3 = (a, b, c) => a / b / c
   t.equal(fnull(div, null, 5)(10), div(10, 5), 2)
-  t.equal(fnull(div, null, 5, null)(10, 2), div(10, 5, 2), 1)
+  t.equal(fnull(div3, null, 5, null)(10, 2), div3(10, 5, 2), 1)
   t.end()
 })
